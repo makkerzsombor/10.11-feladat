ec7aca2b9bfad7db9520773ac5b0750436d0d89b
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-11 13:16:39 +0200

    Fixes done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ class Szazlabu{
     constructor(ujLabak){
         this.labakSzama = ujLabak;
     }
-    // 
+    //
     toString() {
         let string = ' lábú százlábú';
         return this.labakSzama + string;
@@ -14,20 +14,20 @@ class Szazlabu{
         return t.map(e => new Szazlabu(e));
     }    
 }
-//Feladat 5 Nem tiszta
+//Feladat 5 Tiszta
 function labakbolSzazlabuk(t = []){
     return t.map(e => new Szazlabu(e));
 }  
 
 //Feladat 6 Nem Tiszta
-function szazlabuMejelenites(stringID , lista = []){       
+function szazlabuMejelenites(stringID , lista = []){
     lista.forEach((e, index) => {
-        let szuloElem = document.getElementById(stringID); 
+        let szuloElem = document.getElementById(stringID);
         let listaElem = document.createElement('li');
         let listaElemText = document.createElement('p');
         listaElemText.textContent = e.toString();
         listaElem.appendChild(listaElemText);
-        szuloElem.appendChild(listaElem); 
+        szuloElem.appendChild(listaElem);
     });             
 }
 // Nem Tiszta
@@ -39,12 +39,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let lista5 = [];
     // Feladat 2 Nem tiszta
     document.getElementById('fel2').addEventListener('click', () => {    
-        lista5 = [];    
+        lista5 = [];
         lista5.push(lista.filter(e => e % 5 == 0));
         lista5.forEach((e, index) => {
              console.log(index, e);
         })
-    })   
+    })
     // Feladat 3 Nem Tiszta
      document.getElementById('hozzaAd').addEventListener('click', () => {
         lista.push(parseInt(document.getElementById('bekertSzam').value));
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
      // Feladat 4 Nem Tiszta
      let sz = new Szazlabu(30);
      console.log(sz.toString());     
-     // Feladat 6   
+     // Feladat 6
 
      // classon belül szazlabuMejelenites('letrehozas', Szazlabu.labakbolSzazlabuk(lista));
      // classon kivüli: Tiszta
