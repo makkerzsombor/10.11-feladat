06624ccf044b3dc7ea137291ee1c286258781064
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-11 10:58:06 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ let lista5 = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < 20; i++) {
-        lista.push(Math.floor(Math.random() * 30) + 1);
+        lista.push(Math.floor(Math.random() * 30) + 1)
     }
     for (let i = 0; i < lista.length; i++) {
         if (lista[i] % 5 == 0) {
-            lista5.push(lista[i]);
-            console.log(lista[i]);
+            lista5.push(lista[i])
+            console.log(lista[i])
         }
     }
 });
-
