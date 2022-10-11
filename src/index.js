f4d1b6ea677bf2d5fec505fbf2151acc21853d3c
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-11 11:10:46 +0200

    Task 2 completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-let lista = [];
-let lista5 = [];
 
 document.addEventListener('DOMContentLoaded', () => {
+    let lista = [];
     for (let i = 0; i < 20; i++) {
         lista.push(Math.floor(Math.random() * 30) + 1)
     }
-    for (let i = 0; i < lista.length; i++) {
-        if (lista[i] % 5 == 0) {
-            lista5.push(lista[i])
-        }
-    }
-    lista5.forEach((e, index) => {
-         console.log(index, e);
-    })
+    let lista5 = [];
+    // Feladat 2
+    document.getElementById('fel2').addEventListener('click', () => {    
+        lista5 = [];    
+        lista5.push(lista.filter(e => e % 5 == 0));
+        lista5.forEach((e, index) => {
+             console.log(index, e);
+        })
+    })    
 });
