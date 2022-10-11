a2c15c405cf9502897a9fd58baa0e7947ee4793c
Zsombor Makker <makker.zsombor@gmail.com>
2022-10-11 10:55:37 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let lista = [];
+let lista5 = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i < 20; i++) {
+        lista.push(Math.floor(Math.random() * 30) + 1);
+    }
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i] % 5 == 0) {
+            lista5.push(lista[i]);
+            console.log(lista[i]);
+        }
+    }
+});
+
